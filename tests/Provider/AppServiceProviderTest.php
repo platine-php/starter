e29dd4f108deffdffff3d04fcc03c6e3a1b0ddcd
Tony NGUEREZA <nguerezatony@gmail.com>
2023-12-06 11:47:43 +0100

    Add role feature

--- tests/Provider/AppServiceProviderTest.php
+++ tests/Provider/AppServiceProviderTest.php
@@ -18,7 +18,7 @@ class AppServiceProviderTest extends PlatineTestCase
     {
         $app = $this->getMockInstanceMap(Application::class);
 
-        $app->expects($this->exactly(2))
+        $app->expects($this->exactly(3))
                 ->method('bind');
         $o = new AppServiceProvider($app);
         $o->register();
